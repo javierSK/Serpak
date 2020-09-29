ada6e9d30dda5d0f0f188de4699fb228123a00dd
javierSK <38510237+javierSK@users.noreply.github.com>
2020-09-29 20:21:13 -0300

    Add files via upload

--- pa/correo.php
+++ pa/correo.php
@@ -10,9 +10,9 @@
   $telefono = $_POST['telefono'];
 
 $header = "Enviado desde pagina web";
-$mensajeCompleto = "Asunto: ". $asunto. "\n ". "\n ".$mensaje ."\n "."\n ". "\nAtentamente: " . $nombre . "\nDireccion: " . $direccion. "\nTelefono: ". $telefono;
+$mensajeCompleto =  "\nEnviado por : " . $nombre . "\nDireccion: " . $direccion. "\nTelefono: ". $telefono. "\nCorreo electronico". $email. "\nAsunto: ". $asunto. "\n ". "\n ".$mensaje ."\n "."\n ";
 
 mail($destinatario, $asunto, $mensajeCompleto, $header);
 echo"<script>alert('mensaje enviado')</script>";
-echo"<script> setTimeout(\"location.href='i.html'\",1000)</script>";
+echo"<script> setTimeout(\"location.href='index.html'\",1000)</script>";
 ?>
